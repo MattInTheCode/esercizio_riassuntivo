c824e7912ad8be6491fc7e8b509aaf22a8c62344
MattInTheCode <esposito0mattia@gmail.com>
2025-01-31 16:45:42 +0100

    Es controllo di flusso
    nell'esercizio chiediamo all'utente come sta e tramite una serie di if elif e un else generiamo una risposta.

---
+++ esercizio_su_controllo_flusso.py
@@ -0,0 +1,20 @@
+while True:
+    come_stai=input("come stai? bene male non saprei:")
+    
+    if come_stai == "saprei":
+        print("lo dovresti sapere capra")
+    
+    elif come_stai == "bene":
+        print("non me ne frega")
+    
+    elif come_stai == "male":
+        print("non me ne fregava quando stavi bene figurati se stai male")
+    
+    
+    elif come_stai == "esci":
+        break
+        
+    else:
+        print("Sei talmente capra da non capire che devi scegliere fra non sapre, bene o male.")
+        
+    
